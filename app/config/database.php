6bab309ae9186d609dd458befc514609d60fc8e7
shiloh impang <proverbian@gmail.com>
2013-12-16 15:48:34 +0800

    mys second commit

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'database',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'pass',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
